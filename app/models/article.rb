95bb3bedef20ccb5b0d108147ce226ccf6087a56
Dnar <Chanky@list.ru>
2016-03-16 01:13:25 +0400

    Add validate length for article title & text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :text, presence: true, length: { maximum: 4000 }
   has_many :comments
+  belongs_to :username
 
   def subject
     title
--- db/migrate/20160315013451_add_column_to_articles.rb
+++ db/migrate/20160315013451_add_column_to_articles.rb
@@ -1,4 +1,5 @@
 class AddColumnToArticles < ActiveRecord::Migration
   def change
+    add_column :articles, :username, :references
   end
 end
